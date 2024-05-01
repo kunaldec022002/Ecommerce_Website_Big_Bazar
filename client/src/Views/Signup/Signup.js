a588831f27fd3fa4680f0e9882fe2682163b8c89
kunaldec022002 <kunalingale72@gmail.com>
2024-05-01 13:06:57 +0530

    add border

--- client/src/Views/Signup/Signup.js
+++ client/src/Views/Signup/Signup.js
@@ -37,66 +37,70 @@ function Signup(){
                         <div>
                             <Navbar/>
 
-                            <div className="signup-container">
-                                <h1 className="signup-title">Signup</h1>
-                                <div className="input-container">
-                                    <label className="input-label">Name</label>
-                                    <input type="text"
-                                    placeholder="enter your name"
-                                    className="input-field"
-                                    value={name}
-                                    onChange={(e)=>{
-                                        setName(e.target.value)
-                                    }}/>
+                            <div>
+                                
 
+                                <div className="signup-container">
+                                    <h1 className="signup-title">Signup</h1>
+                                    <div className="input-container">
+                                        <label className="input-label">Name</label>
+                                        <input type="text"
+                                        placeholder="enter your name"
+                                        className="input-field"
+                                        value={name}
+                                        onChange={(e)=>{
+                                            setName(e.target.value)
+                                        }}/>
+
+                                    </div>
+
+                                    <div className="input-container">
+                                        <label className="input-label">Email</label>
+                                        <input type="email"
+                                        placeholder="enter your email"
+                                        className="input-field"
+                                        value={email}
+                                        onChange={(e)=>{
+                                            setEmail(e.target.value)
+                                        }}/>
+
+                                    </div>
+
+                                    <div className="input-container">
+                                        <label className="input-label">Mobile</label>
+                                        <input type="text"
+                                        placeholder="enter your Mobile"
+                                        className="input-field"
+                                        value={mobile}
+                                        onChange={(e)=>{
+                                            setMobile(e.target.value)
+                                        }}/>
+
+                                    </div>
+
+                                    <div className="input-container">
+                                        <label className="input-label">Password</label>
+                                        <input type="password"
+                                        placeholder="enter your password"
+                                        className="input-field"
+                                        value={password}
+                                        onChange={(e)=>{
+                                            setPassword(e.target.value)
+                                        }}/>
+
+                                    </div>
+
+                                    <span>Already have account? <Link to='/login' className='links-signup'>Login</Link></span>
+
+                                    <button type="button"
+                                    onClick={signup} 
+                                    className="signup-btn">
+                                        Signup
+                                    </button>
+
+                                
                                 </div>
-
-                                <div className="input-container">
-                                    <label className="input-label">Email</label>
-                                    <input type="email"
-                                    placeholder="enter your email"
-                                    className="input-field"
-                                    value={email}
-                                    onChange={(e)=>{
-                                        setEmail(e.target.value)
-                                    }}/>
-
-                                </div>
-
-                                <div className="input-container">
-                                    <label className="input-label">Mobile</label>
-                                    <input type="text"
-                                    placeholder="enter your Mobile"
-                                    className="input-field"
-                                    value={mobile}
-                                    onChange={(e)=>{
-                                        setMobile(e.target.value)
-                                    }}/>
-
-                                </div>
-
-                                <div className="input-container">
-                                    <label className="input-label">Password</label>
-                                    <input type="password"
-                                    placeholder="enter your password"
-                                    className="input-field"
-                                    value={password}
-                                    onChange={(e)=>{
-                                        setPassword(e.target.value)
-                                    }}/>
-
-                                </div>
-
-                                <span>Already have account? <Link to='/login' className='links-signup'>Login</Link></span>
-
-                                <button type="button"
-                                onClick={signup} 
-                                className="signup-btn">
-                                    Signup
-                                </button>
-
-                            
-                            </div>
+                            </div>  
 
                             
                         </div>
