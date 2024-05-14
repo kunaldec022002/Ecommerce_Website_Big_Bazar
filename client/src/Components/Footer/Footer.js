815f69931f2cb5309165e13c414682cda60ff352
kunaldec022002 <kunalingale72@gmail.com>
2024-05-14 18:49:44 +0530

    deleted two product

---
+++ client/src/Components/Footer/Footer.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Footer.css';
+
+export default function Footer() {
+  return (
+    <div>
+
+      <div className='footer'>
+        <div className='footer-head'>HOME</div>
+        <div className='footer-head'>SIGNUP</div>
+        <div className='footer-head'>LOGIN</div>
+        <div className='footer-head'>SOCIAL MEDIA</div>
+      </div>
+
+      
+    </div>
+  )
+}
